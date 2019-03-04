dba2756f2458db5e01236b045ab9cffff9947d0b
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-03-04 09:49:57 +0100

    Juan pay attention to typo mistakes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   #validanto el contenido de la review que no sea de menos de 20
-  validates :content, length: { minimun: 20}
+
+  validates :content, length: { minimum: 20 }
 end
